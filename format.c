a8cf2338a2736c7336ba5491e207bb31f45b601e
camil <camil.cieslok@stud.thga.de>
2022-05-16 21:09:16 +0200

    format.c mit inhalt gefüllt

---
+++ format.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int ganzzahl = 9;
+double gleitkommazahl = 7,99;
+char character = 'C';
+char string[] ="Programmierung Sommer22";
+
+int main (){
+
+	printf("Ausgabe einer Ganzzahl:		%i\n",ganzzahl);
+	printf("Ausgabe einer Gleitkommazahl:	%.3f\n",gleitkommazahl);
+	printf("Ausgabe eines Characters:	%c\n",character);
+	printf("Ausgabe eines String:		%s\n",string);
+
+
+	return 0;
+
+
+}
